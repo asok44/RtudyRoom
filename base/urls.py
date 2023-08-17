7077df4ad39119a3318fd475b7145c0b6c102424
ashok <sunuwarashok99@gmail.com>
2023-08-17 23:34:33 +0545

    until theme changes

--- base/urls.py
+++ base/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('register/', views.registerUser, name='register'),
 
 
-
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
     path('profile/<str:pk>/', views.userProfile, name='user-profile'),
